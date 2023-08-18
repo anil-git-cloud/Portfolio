407872d93f7bf5d91efd61da1342200784dd18b7
Anil Dhangar <anildhangar2709@gmail.com>
2023-08-18 21:59:26 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+let greet = "Welcome to my Website";
+alert(greet);
+let msg = prompt("Enter Your Name");
+alert("Hey!! Hii" + " "+ msg + " " + "How are you?");
+// alert("Hey!! Hii" + msg + "How are you );
+// document.write(msg + " " + "You Can Explore Our Website");
