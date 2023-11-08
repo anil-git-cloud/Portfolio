29eb4bf163abfed58aa42ea6c3aca48e80cf801d
Anil Dhangar <anildhangar2709@gmail.com>
2023-11-08 19:54:41 +0530

    last

--- portfolio.js
+++
@@ -1,6 +0,0 @@
-let greet = "Welcome to my Website";
-alert(greet);
-let msg = prompt("Enter Your Name");
-alert("Hey!! Hii" + " "+ msg + " " + "How are you?");
-// alert("Hey!! Hii" + msg + "How are you );
-// document.write(msg + " " + "You Can Explore Our Website");
